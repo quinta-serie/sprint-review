c3ae9764c2f7cb0c4cf52c88645e3c68505b8740
leonardo.goncalves <leogoncalves.contato@gmail.com>
2024-06-14 10:26:44 -0300

    🚧 initial page working progress

--- packages/projects/client/src/pages/Signin/Signin.tsx
+++ packages/projects/client/src/pages/Signin/Signin.tsx
@@ -9,8 +9,11 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import CardContent from '@mui/material/CardContent';
 import GoogleIcon from '@mui/icons-material/Google';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 import { auth } from '@/services/core';
+import IconButton from '@mui/material/IconButton';
 
 export default function Signin() {
     const navigate = useNavigate();
@@ -30,29 +33,37 @@ export default function Signin() {
         }}>
             <Container sx={{ width: 500 }}>
                 <Zoom in>
-                    <Card>
-                        <CardContent>
-                            <Stack spacing={2}>
-                                <Box>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        Signin
-                                    </Typography>
-                                    <Typography gutterBottom variant="body2" component="p">
-                                        Use your account google to signin
-                                    </Typography>
-                                </Box>
-                                <Button
-                                    fullWidth
-                                    size="large"
-                                    variant="contained"
-                                    startIcon={<GoogleIcon />}
-                                    onClick={signinWithGoogle}
-                                >
-                                    Sign in with google
-                                </Button>
-                            </Stack>
-                        </CardContent>
-                    </Card>
+                    <Box>
+                        <Card>
+                            <CardContent>
+                                <Stack spacing={2}>
+                                    <Box>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            Signin
+                                        </Typography>
+                                        <Typography gutterBottom variant="body2" component="p">
+                                            Use your account google to signin
+                                        </Typography>
+                                    </Box>
+                                    <Button
+                                        fullWidth
+                                        size="large"
+                                        variant="contained"
+                                        startIcon={<GoogleIcon />}
+                                        onClick={signinWithGoogle}
+                                    >
+                                        Sign in with google
+                                    </Button>
+                                </Stack>
+                            </CardContent>
+                        </Card>
+                        <Typography
+                            sx={{ mt: 2, textAlign: 'center' }}
+                            variant="body2"
+                        >
+                            Copyright © 2024, Sprint Retro
+                        </Typography>
+                    </Box>
                 </Zoom>
             </Container>
         </ Box>
