8151ccaf147ad57b592415438f0610fb18b4dc60
leonardo.goncalves <leogoncalves.contato@gmail.com>
2024-06-14 17:21:44 -0300

    💄 add logo component

---
+++ packages/projects/client/src/components/Logo/Logo.tsx
@@ -0,0 +1,32 @@
+import type { HTMLAttributes } from 'react';
+
+import { useTheme } from '@mui/material/styles';
+
+import './Logo.scss';
+
+interface LogoProps extends HTMLAttributes<HTMLElement> {
+    tag?: React.ElementType;
+    color?: 'primary' | 'secondary' | 'contrast' | 'default';
+}
+export default function Logo({ tag = 'div', color = 'default', ...props }: LogoProps) {
+    const { palette } = useTheme();
+
+    const CustomTag = tag;
+
+    const COLOR_MAP = {
+        primary: palette.primary.main,
+        secondary: palette.secondary.main,
+        default: palette.text.primary,
+        contrast: palette.primary.contrastText,
+    };
+
+    return (
+        <CustomTag
+            {...props}
+            style={{ ...props.style, color: COLOR_MAP[color] }}
+            className={[props.className, 'pacifico-regular'].join(' ')}
+        >
+            Sprint Review
+        </CustomTag>
+    );
+}
