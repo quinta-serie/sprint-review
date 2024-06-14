bd1aa8d974e438a69e3d09f1f35e63a99e1c79b9
leonardo.goncalves <leogoncalves.contato@gmail.com>
2024-06-14 17:23:01 -0300

    🎨 add theme structure

--- packages/projects/client/src/App.tsx
+++ packages/projects/client/src/App.tsx
@@ -1,11 +1,19 @@
 import { Outlet } from 'react-router-dom';
 
+import type { PaletteMode } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+import Layout from '@/layout/Default';
+
+import theme from './theme';
+
 function App() {
+    const getTokens = (mode: PaletteMode) => { return createTheme(theme[mode]); };
     return (
-        <ThemeProvider theme={createTheme()}>
-            <Outlet />
+        <ThemeProvider theme={getTokens('light')}>
+            <Layout>
+                <Outlet />
+            </Layout>
         </ThemeProvider>
     );
 }
