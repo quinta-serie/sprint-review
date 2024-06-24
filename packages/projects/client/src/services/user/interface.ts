1d359a241f3161a870aaf0a7349797bedad75f17
leonardo.goncalves <leogoncalves.contato@gmail.com>
2024-06-24 19:32:10 -0300

    feat: add user service

---
+++ packages/projects/client/src/services/user/interface.ts
@@ -0,0 +1,6 @@
+export interface UserData {
+    name: string;
+    email: string;
+    user_id: string;
+    picture: string;
+}
---
+++ packages/projects/client/src/services/user/user.ts
@@ -0,0 +1,39 @@
+import type DB from '@/services/db';
+import { decode } from '@/utils/jwt';
+import { Cookies } from '@/utils/cookies';
+
+import type { UserData } from './interface';
+
+export default class User {
+    private static PATH = 'users';
+    private cookies = new Cookies();
+
+    constructor(private db: DB) { }
+
+    get current(): UserData {
+        const data = decode<UserData>(this.cookies.get('access_token'));
+
+        return {
+            name: data.name,
+            email: data.email,
+            picture: data.picture,
+            user_id: data.user_id,
+        };
+    }
+
+    getUserByEmail(email: string) {
+        return this.db.getItem<UserData>({
+            path: User.PATH,
+            pathSegments: [],
+            filters: [{ field: 'email', operator: '==', value: email }],
+        });
+    }
+
+    createUser() {
+        return this.db.setItem<UserData>({
+            path: User.PATH,
+            data: this.current,
+            pathSegments: [this.current.email],
+        });
+    }
+}
