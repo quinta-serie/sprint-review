1811ac7ec051a51c5bdba341b2deeef575c1b266
leonardo.goncalves <leogoncalves.contato@gmail.com>
2024-06-24 19:34:11 -0300

    feat: add notistack

--- packages/projects/client/src/App.tsx
+++ packages/projects/client/src/App.tsx
@@ -1,7 +1,12 @@
 import { Outlet } from 'react-router-dom';
 
+import { SnackbarProvider, closeSnackbar } from 'notistack';
+
 import type { PaletteMode } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import CloseIcon from '@mui/icons-material/Close';
 
 import Layout from '@/layout/Default';
 
@@ -11,10 +16,26 @@ function App() {
     const getTokens = (mode: PaletteMode) => { return createTheme(theme[mode]); };
     return (
         <ThemeProvider theme={getTokens('light')}>
-            <Layout>
-                <Outlet />
-            </Layout>
-        </ThemeProvider>
+            <SnackbarProvider
+                maxSnack={3}
+                hideIconVariant={false}
+                autoHideDuration={3000}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                action={(key) => (
+                    <IconButton
+                        size="small"
+                        sx={{ color: 'white' }}
+                        onClick={() => { closeSnackbar(key); }}
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                )}
+            >
+                <Layout>
+                    <Outlet />
+                </Layout>
+            </SnackbarProvider>
+        </ThemeProvider >
     );
 }
 
