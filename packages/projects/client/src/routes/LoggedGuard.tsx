86645f2fbe4e730577a8ddb399c82dbecda3d552
leonardo.goncalves <leogoncalves.contato@gmail.com>
2024-07-25 18:21:39 -0300

    feat: improve logged guard

--- packages/projects/client/src/routes/LoggedGuard.tsx
+++ packages/projects/client/src/routes/LoggedGuard.tsx
@@ -4,12 +4,13 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { auth } from '@/services/core';
 
 export const UserLoggedOutGuard = ({ children }: React.PropsWithChildren<unknown>) => {
+    const pathRedirect = '/auth/signin';
     const navigate = useNavigate();
     const location = useLocation();
 
     useEffect(() => {
-        if (!auth.access_token) {
-            navigate('/auth');
+        if (!auth.access_token && location.pathname !== pathRedirect) {
+            navigate('/auth/signin');
             return;
         }
     }, [location]);
